9edb7500a8504fc0a38cd62a4af21c59725efdc7
llsmith05 <llsmith05@gmail.com>
2014-02-28 15:22:46 -0700

    Kinect Toolkit and initial demo window
    Full solution for demo and included Toolkit project

---
+++ KinectDemo/KinectDemo/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        KinectSensor _sensor;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //can't get the event handler for SensorChooser to generate here. need documentation for UI version in SDK 1.5
+            //kinectSensorChooser1.KinectSensorChooser.Status ;
+
+            //initialize all streams and start sensor
+            _sensor.ColorStream.Enable();
+            _sensor.DepthStream.Enable();
+            _sensor.SkeletonStream.Enable();
+            _sensor.AllFramesReady += _sensor_AllFramesReady;
+            _sensor.Start();
+        }
+
+        void _sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                sensor.Stop();
+                sensor.AudioSource.Stop();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopKinect(_sensor);
+        }
+    }
+}
